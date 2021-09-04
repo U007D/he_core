709ac6c7120d4aee32cbcf99da8fef786630f5db
U007D <curly@humanenginuity.com>
2021-09-03 20:51:57 -0700

    wip: update pll impl: red; TODO: get PLL's to lock

--- src/processor/riscv/rv64/core_clock.rs
+++ src/processor/riscv/rv64/core_clock.rs
@@ -8,25 +8,25 @@ pub fn init(peripherals: &mut Peripherals) {
     HfXOsc = 1,
   }
 
-  // TODO: Determine if hart must be switched to external oscillator while setting core_clk, determine `hfxoscen` sense
-  // // Enable hfx (high-frequency external) oscillator
-  // peripherals.PRCI.hfxosccfg.modify(|_, w| w.hfxoscen().set_bit());
-  // // Busy wait for frequency lock
-  // while peripherals.PRCI.hfxosccfg.read().hfxoscrdy().bit_is_clear() {}
-  // // (Temporarily) select external high frequency clock as coreclk source
-  // peripherals.PRCI.core_clk_sel_reg.modify(|_, w| unsafe { w.bits(CoreClockSource::HfXOsc as _) });
+  // Enable hfx (high-frequency external) oscillator
+  peripherals.PRCI.hfxosccfg.modify(|_, w| w.hfxoscen().set_bit());
+  // Busy wait for frequency lock
+  while peripherals.PRCI.hfxosccfg.read().hfxoscrdy().bit_is_clear() {}
+  // (Temporarily) select external high frequency clock as coreclk source
+  peripherals.PRCI.core_clk_sel_reg.modify(|_, w| unsafe { w.bits(CoreClockSource::HfXOsc as _) });
+
+  // Disable PLL (required for reconfiguration, active high?)
+  peripherals.PRCI.core_pllcfg.modify(|_, w| w.pllbypass().clear_bit());
 
   // Configure `corepll` for 1 GHz (see fu740-c000-manual-v1p2, p. 84)
-  peripherals.PRCI.core_pllcfg.modify(|_, w| unsafe {
-    w.pllr().bits(0);
-    w.pllf().bits(76);
-    w.pllq().bits(2)
-  });
+  peripherals.PRCI.core_pllcfg.modify(|_, w| unsafe { w.pllr().bits(0).pllf().bits(76).pllq().bits(2) });
+
+  // Enable PLL with new settings (active high?)
+  peripherals.PRCI.core_pllcfg.modify(|_, w| w.pllbypass().set_bit());
+
   // Busy wait for pll lock
-  // TODO: determine `plllock` sense: active low?
-  while peripherals.PRCI.core_pllcfg.read().plllock().bit_is_set() {}
+  while peripherals.PRCI.core_pllcfg.read().plllock().bit_is_clear() {}
 
   // Select coreclkpll as coreclk source
   peripherals.PRCI.core_clk_sel_reg.modify(|_, w| unsafe { w.bits(CoreClockSource::CoreClkPll as _) });
 }
-
